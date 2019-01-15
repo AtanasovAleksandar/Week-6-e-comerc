dc62c837ab2b00638e2043e5d85afed746730eb5
Aleksandar Atanasov <aleksandar.atanasov@office.it-labs.com>
2019-01-15 13:21:52 +0100

    design done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListCategoryComponent } from './content/list-category/list-category.component';
 import { ProductsCategoryComponent } from './content/products-category/products-category.component';
+import { AddCategoryComponent } from './content/add-category/add-category.component';
 
 const routes: Routes = [
   {path: 'Category' , component: ListCategoryComponent },
+  {path: 'Add-Category' , component: AddCategoryComponent },
   {path: 'Products' , component: ProductsCategoryComponent },
   {path: '' , redirectTo: "Customers/1/none" , pathMatch: 'full'},
   {path: '**' , redirectTo: "Customers/1/none" , pathMatch:'full'}
