cc5127a5688baa1bf777358c6020a389eb8ffe73
Aleksandar Atanasov <aleksandaratanasov43@gmail.com>
2019-01-29 22:55:34 +0100

    upload part fix

--- src/app/components/products/add-products/add-products.component.ts
+++ src/app/components/products/add-products/add-products.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
-import { AngularFireStorage } from 'angularfire2/storage';
+import {
+  AngularFireStorage,
+  AngularFireStorageReference, AngularFireUploadTask
+} from 'angularfire2/storage';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-products',
@@ -8,22 +14,56 @@ import { AngularFireStorage } from 'angularfire2/storage';
   styleUrls: ['./add-products.component.scss']
 })
 export class AddProductsComponent implements OnInit {
-  file: File = null;
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+  uploadState: Observable<void>;
+  uploadProgress: Observable<number>;
+  downloadURL: Observable<string>;
+  downloadLink: Object;
+  complite: any;
 
-  constructor(public productService: ProductsService, private afStorage: AngularFireStorage) {}
+  constructor(public http: HttpClient, public productService: ProductsService, private afStorage: AngularFireStorage) { }
 
   ngOnInit() {
   }
 
-  onFileChanged(event) {
-    const file = <File> event.target.files[0]
-    this.file = file;
-    console.log(this.file)
+  upload(event) {
+    const id = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(id);
+    this.task = this.ref.put(event.target.files[0]);
+    this.uploadState = this.task.snapshotChanges().pipe(map(s => this.getUrl(s.metadata) ));
+    this.uploadProgress = this.task.percentageChanges();
+     
+      // this.downloadURL = this.ref.getDownloadURL();
+    //   console.log(this.downloadURL)
+    //  const link = this.ref.child(id).getDownloadUrl().then(function(url) {
+    //    console.log(url)
+    //  });
+
+    // this.ref.snapshot.ref.getDownloadURL().then(downloadURL => {
+    //   console.log('File available at', downloadURL);
+    //   fileUpload.url = downloadURL;
+    //   fileUpload.name = fileUpload.file.name;
+    //   this.saveFileData(fileUpload);
+    // });
   }
 
-  fileUpload() {
-    this.afStorage.upload('/upload/to/this-path', this.file);
+  getUrl(s) {
+    if (s != null) {
+      this.downloadURL = this.ref.getDownloadURL();
     }
   }
+
+    // Create a reference to the file we want to download
+    // if (this.uploadProgress) {
+    //   var starsRef = this.ref.child(id);
+
+    //   // Get the download URL
+    //   starsRef.getDownloadURL().then(function (url) {
+        // Insert url into an <img> tag to "download"
+  //     });
+  //   }
+  // }
+}
 
 
